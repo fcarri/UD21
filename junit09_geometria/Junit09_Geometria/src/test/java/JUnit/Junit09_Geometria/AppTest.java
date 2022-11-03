04ac182a6204f47fd3c0137d1eb3f9d12a865853
fcarri <114770670+fcarri@users.noreply.github.com>
2022-11-03 14:14:37 +0100

    commit_21

---
+++ junit09_geometria/Junit09_Geometria/src/test/java/JUnit/Junit09_Geometria/AppTest.java
@@ -0,0 +1,14 @@
+package JUnit.Junit09_Geometria;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import JUnit.Junit09_Geometria.*;
+
+class AppTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ junit09_geometria/Junit09_Geometria/src/test/java/JUnit/Junit09_Geometria/dto/geometria.java
@@ -0,0 +1,14 @@
+package JUnit.Junit09_Geometria.dto;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import JUnit.Junit09_Geometria.dto.*;
+
+class geometria {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
